c64615e1fc98df24bc6bc25d0bf30fd325ca7ab4
magdamagdy <1701070@eng.asu.edu.eg>
2022-11-03 17:39:33 +0200

    changes

--- transportation-booking-system/src/main/java/com/exalt/transportationbookingsystem/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.exalt.transportationbookingsystem;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
--- transportation-booking-system/src/main/java/com/exalt/transportationbookingsystem/soapclientautogenerated/tripSoapClient/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://client.soap.service.transportationbookingsystem.exalt.com/")
-package tripSoapClient;
